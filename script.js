31e8dfc9099d5e899a3b40f35a0451cccaa095b6
benbgw <77581854+benbgw@users.noreply.github.com>
2021-02-05 10:55:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-body {background-color:black;
-    font-family:arial;
-    font-color:white}
+function compute(){
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rangeRate").value;
+    var years = document.getElementById("years").value;
+    var fut = convertYears(years);
+    var interest = principal * years * rate / 100;
+    if(validateForm()){
+        document.getElementById("result").innerHTML=
+        "If you deposit <mark>"+ principal +"</mark>,<br/>"+
+        " at an interest rate of <mark>"+ rate +"</mark>.<br/>"+
+        " You will receive an amount of <mark>"+ interest +"</mark>,<br/>"+
+        " in the year <mark>" + fut+"</mark>";
+    }
+}
 
-h1{color:grey;
-    font-family:verdana}
+function readRate(val){
+    document.getElementById("rate").innerHTML=val;
+}
 
-.maindiv{
-    background-color:white;
-    color: black;
-    width: 300px;
-    padding: 20px;
-    border-radius: 25px;
-    margin: auto;
+function convertYears(years){
+    var date = new Date(); 
+    var year = date.getFullYear();
+    var future = parseInt(year) + parseInt(years);
+    return future;
+}
+    
+
+function validateForm() {
+  var principal = document.getElementById("principal").value;
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus(); 
+    return false;
+  }
+  return true
 }
         
